ff6d55e18372d658d6d41359c9dcf2499fa21c24
Reynaldo R. Martinez P <tigerlinux@gmail.com>
2013-12-23 18:49:47 -0430

    Commit para version 1.1.4 "Mr. Cat"
    - Correccion menor en script principal de instalacion.

--- main-installer.sh
+++ main-installer.sh
@@ -8,7 +8,7 @@
 # Primera versión (Havana - Debian7): Octubre 30 del 2013
 #
 # Script principal
-# Versión 1.1.3 "Mr. Cat"
+# Versión 1.1.4 "Mr. Cat"
 # 23 de Diciembre del 2013
 #
 
@@ -40,7 +40,7 @@ case $1 in
 	echo "INSTALADOR DE OPENSTACK HAVANA PARA DEBIAN 7"
 	echo "Realizado por Reynaldo R. Martinez P."
 	echo "E-Mail: TigerLinux@Gmail.com"
-	echo "Versión 1.1.2 \"Mr. Cat\" - Noviembre 06, 2013"
+	echo "Versión 1.1.4 \"Mr. Cat\" - Diciembre 23, 2013"
 	echo ""
 	echo "Se verificaran los prerequisitos"
 	echo "Si alguno de los prerequisitos falla, se informará y se detendrá el proceso"
